3fa0e3076e952af17b7c86486f7ea74415b2007e
Brecht Gorissen <brecht.gorissen@gmail.com>
2018-12-18 14:09:59 +0100

    added index and basket templates + corresponding controllers

---
+++ src/main/java/gorissen/vleminckx/timco/controllers/IndexController.java
@@ -0,0 +1,28 @@
+package gorissen.vleminckx.timco.controllers;
+
+import gorissen.vleminckx.timco.model.Product;
+import gorissen.vleminckx.timco.model.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private ProductRepository repo;
+
+    @ModelAttribute("all")
+    public Iterable<Product> getAll(){
+        return repo.findAll();
+    }
+
+    @RequestMapping(value = {"/index","/"}, method = RequestMethod.GET)
+    public String showIndex(ModelMap map){
+
+        return "index";
+    }
+}
