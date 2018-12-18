b15dec93c43dd513d576a1a348ddbc085f1d316d
Brecht Gorissen <brecht.gorissen@gmail.com>
2018-12-18 16:00:11 +0100

    added basket enum added functionality to add products to basket

--- src/main/java/gorissen/vleminckx/timco/controllers/BasketController.java
+++ src/main/java/gorissen/vleminckx/timco/controllers/BasketController.java
@@ -1,6 +1,7 @@
 //Brecht
 package gorissen.vleminckx.timco.controllers;
 
+import gorissen.vleminckx.timco.model.Basket;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ public class BasketController {
 
     @RequestMapping(value = "/basket", method = RequestMethod.GET)
     public String showBasket(){
-
+        Basket.INSTANCE.getBasket();
         return "basket";
     }
 }
--- src/main/java/gorissen/vleminckx/timco/controllers/IndexController.java
+++ src/main/java/gorissen/vleminckx/timco/controllers/IndexController.java
@@ -1,14 +1,18 @@
 //Brecht
 package gorissen.vleminckx.timco.controllers;
 
+import gorissen.vleminckx.timco.model.Basket;
 import gorissen.vleminckx.timco.model.Product;
 import gorissen.vleminckx.timco.model.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
 
 @Controller
 public class IndexController {
@@ -25,5 +29,15 @@ public class IndexController {
     public String showIndex(ModelMap map){
 
         return "index";
+    }
+
+    @RequestMapping(value = "index/add/{id}", method = RequestMethod.GET)
+    public String addToBasket(@PathVariable (value = "id") int id){
+        return "redirect:/index";
+    }
+    @RequestMapping(value = "index/add/{id}", method = RequestMethod.POST)
+    public String addToBasket(@PathVariable (value = "id") int id, ArrayList<Product> basket){
+        Basket.INSTANCE.setBasket(basket);
+        return "redirect:/index";
     }
 }
