a7720c57f624e2111244f152479db666f9490312
Bethje <liesbeth.vleminckx@gmail.com>
2018-12-18 14:17:45 +0100

    Added Concstructor + Getters & Setters to Entity

--- src/main/java/gorissen/vleminckx/timco/model/Product.java
+++ src/main/java/gorissen/vleminckx/timco/model/Product.java
@@ -32,5 +32,54 @@ public class Product {
     @NotBlank
     private String omschrijving;
 
+    public Product() {
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(double prijs) {
+        this.prijs = prijs;
+    }
+
+    public Diersoort getDiersoort() {
+        return diersoort;
+    }
+
+    public void setDiersoort(Diersoort diersoort) {
+        this.diersoort = diersoort;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+
+    public void setOmschrijving(String omschrijving) {
+        this.omschrijving = omschrijving;
+    }
 }
