a3f591b6e063f9c910d996dade0b14b239426586
Brecht Gorissen <brecht.gorissen@gmail.com>
2018-12-18 12:06:15 +0100

    Initial commit Empty Spring initializer project

---
+++ src/main/java/gorissen/vleminckx/timco/TimcoApplication.java
@@ -0,0 +1,14 @@
+package gorissen.vleminckx.timco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimcoApplication.class, args);
+    }
+
+}
+
