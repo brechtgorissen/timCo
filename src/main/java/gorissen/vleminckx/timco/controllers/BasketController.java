314eae387c23c77336f4aaac49732ba7b2de61ab
Brecht Gorissen <brecht.gorissen@gmail.com>
2018-12-20 16:46:12 +0100

    added code documentation to controllers

--- src/main/java/gorissen/vleminckx/timco/controllers/BasketController.java
+++ src/main/java/gorissen/vleminckx/timco/controllers/BasketController.java
@@ -1,4 +1,3 @@
-//Brecht
 package gorissen.vleminckx.timco.controllers;
 
 import gorissen.vleminckx.timco.model.Basket;
@@ -12,24 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
-
+//controller class for control of basket.html page.
 @Controller
 public class BasketController {
 
     @Autowired
     private ProductRepository repo;
-
+    //model attribute used in html to show product information from products added to basket
     @ModelAttribute("basket")
     public ArrayList<Product> showNewBasket() {
         return Basket.INSTANCE.getNewBasket();
     }
-
+    //request mapping to return basket.html after uri request /basket
     @RequestMapping(value = "/basket", method = RequestMethod.GET)
     public String showBasket() {
 
         return "basket";
     }
-
+    //request mapping to delete product from basket by product id and redirect to basket.html
     @RequestMapping(value = "/basket/delete/{id}", method = RequestMethod.GET)
     public String deleteFromBasket(@PathVariable(value = "id") int id) {
         Basket.INSTANCE.deleteFromBasket(repo.findById(id).get());
--- src/main/java/gorissen/vleminckx/timco/controllers/CheckoutController.java
+++ src/main/java/gorissen/vleminckx/timco/controllers/CheckoutController.java
@@ -1,5 +1,3 @@
-//Liesbeth
-
 package gorissen.vleminckx.timco.controllers;
 
 import gorissen.vleminckx.timco.model.User;
@@ -10,20 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
-
+// controller class for control of checkout.html page
 @Controller
 public class CheckoutController {
-
+    //request mapping to return checkout.html after uri request /checkout
     @RequestMapping(value = {"/checkout"}, method = RequestMethod.GET)
     public String showCheckout() {
         return "checkout";
     }
-
+    //model attribute used in checkout.html to create form input fields for user
     @ModelAttribute("newUser")
     public User newUser() {
         return new User();
     }
-
+    //request mapping used to validate user form input
+    //if valid returns confirmation.html page
+    //if not valid returns back to checkout.html with error messages defined in fields from User class
     @RequestMapping(value = "/confirmation", method = RequestMethod.POST)
     public String checkForErrors(@ModelAttribute("newUser") @Valid User newUser,
                                  BindingResult bindingResult) {
